e777754402c55d99a27f54f553d769b8d7d01de6
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-01-25 14:04:03 +0530

    rosy: update prop.mk

--- prop.mk
+++ prop.mk
@@ -80,9 +80,9 @@ debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
+debug.sf.latch_unsignaled=0 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
-persist.debug.wfd.enable=1 \
 persist.demo.hdmirotationlock=false \
 persist.hwc.enable_vds=1 \
 persist.hwc.mdpcomp.enable=true \
@@ -91,8 +91,8 @@ ro.qualcomm.cabl=0 \
 ro.sf.lcd_density=320 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
-vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
+vendor.display.disable_skip_validate=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
 sys.display-size=1920x1080
 
@@ -120,6 +120,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
+
+#HWUI properties
+ro.hwui.texture_cache_size=72
+ro.hwui.layer_cache_size=48
+ro.hwui.r_buffer_cache_size=8
+ro.hwui.path_cache_size=32
+ro.hwui.gradient_cache_size=1
+ro.hwui.drop_shadow_cache_size=6
+ro.hwui.texture_cache_flushrate=0.4
+ro.hwui.text_small_cache_width=1024
+ro.hwui.text_small_cache_height=1024
+ro.hwui.text_large_cache_width=2048
+ro.hwui.text_large_cache_height=1024
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -164,7 +177,7 @@ persist.vendor.radio.nitz_sons_3=""
 
 # Optimize
 PRODUCT_PROPERTY_OVERRIDES += \
-sys.use_fifo_ui=1
+sys.use_fifo_ui=0
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -196,7 +209,7 @@ ril.subscription.types=NV,RUIM \
 rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=20 \
+ro.telephony.default_network=22,22 \
 vendor.service.qti.ims.enabled=1 \
 telephony.lteOnCdmaDevice=1 \
 persist.vendor.ims.dropset_feature=0 \
